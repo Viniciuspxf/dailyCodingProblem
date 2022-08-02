81129ed8f6f5fac86efe4f80b95895b440c75bf4
Vinicius Frota <viniciuspxfrota@gmail.com>
2022-08-01 21:15:15 -0300

    add first problem

---
+++ 1/solution.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool solution(vector<int> list, int k) {
+    map<int, int> occurrences;
+    int anotherNumber;
+
+    for (int i = 0; i < list.size(); i++) {
+        anotherNumber = k - list[i];
+
+        if (occurrences.find(anotherNumber) != occurrences.end())
+            return true;
+
+        if (occurrences.find(list[i]) == occurrences.end())
+            occurrences[list[i]]++;
+        else
+            occurrences[list[i]] = 1;
+
+        
+    }
+
+    return false;
+}
+
+int main() {
+    vector<int> list;
+    int input;
+
+    while (cin >> input) {
+        list.push_back(input);
+    }
+
+    list.pop_back();
+    
+    if (solution(list, input)) 
+        cout << "True\n";
+    else 
+        cout << "False\n";
+}
