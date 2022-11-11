0fbadfd93244f2503a2cf99ee701ddb54ed360df
Vinicius Frota <viniciuspxfrota@gmail.com>
2022-11-11 14:02:32 -0300

    add stair case challenge by amazon

---
+++ amazon/hard/staircase.py
@@ -0,0 +1,34 @@
+# This problem was asked by Amazon.
+
+# There exists a staircase with N steps, and you can climb up either 1 or 2 steps at a time. Given N, write a function that returns the number of unique ways you can climb the staircase. The order of the steps matters.
+
+# For example, if N is 4, then there are 5 unique ways:
+
+# 1, 1, 1, 1
+# 2, 1, 1
+# 1, 2, 1
+# 1, 1, 2
+# 2, 2
+# What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
+
+
+def staircase(n, steps = [], possible_steps = {1,2}):
+  if n == 0:
+    print(steps)
+    return
+
+  if n < 0:
+    return
+
+  for i in possible_steps:
+    steps.append(i)
+    staircase(n - i, steps, possible_steps)
+    steps.pop()
+
+for i in range(1, 6):
+  print("---------------------TEST", i ,"----------------------")
+  staircase(i)
+
+for i in range(1, 6):
+  print("---------------------TEST", i ,"----------------------")
+  staircase(i, possible_steps={1, 3, 5})
